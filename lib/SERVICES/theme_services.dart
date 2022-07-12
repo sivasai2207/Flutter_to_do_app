b7876404ce10a9ab3da2a36d6be96dfdd739a5da
sivasai2207 <sivasai.reddy0@gmail.com>
2022-07-12 20:48:42 +0530

    basic push

--- lib/SERVICES/theme_services.dart
+++ lib/SERVICES/theme_services.dart
@@ -1,3 +1,11 @@
-class ThemeService{
-  
+import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+
+class ThemeService {
+  final _box = GetStorage();
+  final _key = 'isDarkMode';
+
+// This is used for checking the the value of the key if it is true it will execute or else it will return false.
+  bool _loadThemeFromBox() => _box.read(_key) ?? false;
+  ThemeMode get theme => _loadThemeFromBox() ? ThemeMode.dark : ThemeMode.light;
 }
